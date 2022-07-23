04eebc90ce8008275f869df5e5bb50d00e4c33e0
Om Duragkar <80326954+omduragkar@users.noreply.github.com>
2022-07-23 20:08:24 +0530

    added changes

--- client/src/redux/actions/noteDispatch.js
+++ client/src/redux/actions/noteDispatch.js
@@ -7,27 +7,4 @@ import { setNote } from "../reducers/notesReducers";
 
 export const getAllnotes = (token)=>async (dispatch)=>{
     
-    dispatch(changeloading(true))
-    try{
-
-        let notesData = await axios.get("http://localhost:5000/api/note/getNotes", {
-            headers:{
-                "Content-Type": "application/json",
-                "Authorization": token
-            }
-        })
-        console.log(notesData.data.findNote);
-        dispatch(setNote(notesData.data.findNote));
-        dispatch(changeloading(false));
-    }catch(err){
-        console.log(err.response)
-        dispatch(changeloading(false));
-        dispatch(changeError({
-            err:true,
-            message:err?.response?.data?err?.response.data.message:"Error in connection",
-            type:err?.response?.data?err.response.data.type:null
-        }))
-
-    }
-    
 }
